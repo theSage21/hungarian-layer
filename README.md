ca1ea3a4e0c0a6ccab7f9ad66a74c84f81451197
Arjoonn Sharma <arjoonn.94@gmail.com>
2018-05-18 21:41:05 +0530

    docs

--- README.md
+++ README.md
@@ -2,3 +2,31 @@ Hungarian Layer
 ==============
 
 Pytorch implementation of the Hungarian layer as described in <https://arxiv.org/abs/1712.02555>
+
+Usage
+-----
+
+
+```python
+import numpy as np
+
+x = [[[0, 1], [0, 0], [1, 0]],
+     [[0, 1], [0, 0], [1, 0]],
+     [[0, 1], [0, 0], [1, 0]],
+     [[0, 1], [0, 0], [1, 0]]
+     ]
+y = [[[0, 1], [0, 0]],
+     [[0, 1], [0, 0]],
+     [[0, 1], [0, 0]],
+     [[0, 1], [0, 0]]]
+x = np.array(x).astype(np.float64)
+y = np.array(y).astype(np.float64)
+
+x = torch.from_numpy(x)  # (4, 3, 2)
+y = torch.from_numpy(y)  # (4, 2, 2)
+
+hung = Hungarian()
+a = hung(x, y)
+print(a.shape)  #   (4, 3, 2)
+print(a)
+```
--- hungarian.py
+++ hungarian.py
@@ -10,36 +10,41 @@ class Hungarian(nn.Module):
     def forward(self, seq1, seq2):
         # B, S1, D
         # B, S2, D
-        R1, R2 = [], []
+        R1 = []
         for s1, s2 in zip(torch.unbind(seq1, 0),
                           torch.unbind(seq2, 0)):
             cost = []
             for i in s1:
                 row = []
                 for j in s2:
-                    d = torch.norm((i - j), 2)
-                    d = torch.squeeze(d)  # B, 1
+                    d = torch.squeeze(torch.norm((i - j), 2))
                     row.append(d)
                 row = torch.squeeze(torch.stack(row))
                 cost.append(row)
-            cost = torch.stack(cost, 1)
-            cost = torch.squeeze(cost)
-            cost = cost.numpy()
+            cost = torch.stack(cost, 0).numpy()
             row_ind, col_ind = linear_sum_assignment(cost)
-            R1.append(torch.stack([s1[i, :] for i in row_ind], 1))
-            R2.append(torch.stack([s2[i, :] for i in col_ind], 1))
+            row_remain = set(range(cost.shape[0])) - set(row_ind)
+            col_remain = set(range(cost.shape[1])) - set(col_ind)
+            row_ind = list(row_ind) + list(sorted(row_remain))
+            col_ind = list(col_ind) + list(sorted(col_remain))
+            row = torch.stack([s1[i, :] for i in row_ind], 0)
+            R1.append(row)
 
-        reorder_1 = torch.stack(R1, 0)
-        reorder_2 = torch.stack(R2, 0)
-        return reorder_1, reorder_2
+        reorder = torch.stack(R1, 0)
+        return reorder
 
 
 if __name__ == '__main__':
     import numpy as np
 
     x = [[[0, 1], [0, 0], [1, 0]],
-         [[0, 1], [0, 0], [1, 0]]]
+         [[0, 1], [0, 0], [1, 0]],
+         [[0, 1], [0, 0], [1, 0]],
+         [[0, 1], [0, 0], [1, 0]]
+         ]
     y = [[[0, 1], [0, 0]],
+         [[0, 1], [0, 0]],
+         [[0, 1], [0, 0]],
          [[0, 1], [0, 0]]]
     x = np.array(x).astype(np.float64)
     y = np.array(y).astype(np.float64)
@@ -48,6 +53,6 @@ if __name__ == '__main__':
     y = torch.from_numpy(y)
 
     hung = Hungarian()
-    a, b = hung(x, y)
+    a = hung(x, y)
+    print(a.shape)
     print(a)
-    print(b)
