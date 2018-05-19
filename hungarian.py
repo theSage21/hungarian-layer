24fcf02cc7d610968c43d104f2b5cab5d96c7d60
Arjoonn Sharma <arjoonn.94@gmail.com>
2018-05-19 14:52:46 +0530

    concatenation + cosine similarity

--- hungarian.py
+++ hungarian.py
@@ -6,32 +6,43 @@ from scipy.optimize import linear_sum_assignment
 class Hungarian(nn.Module):
     def __init__(self):
         super().__init__()
+        self.cosine = torch.nn.modules.distance.CosineSimilarity()
 
     def forward(self, seq1, seq2):
         # B, S1, D
         # B, S2, D
-        R1 = []
-        for s1, s2 in zip(torch.unbind(seq1, 0),
-                          torch.unbind(seq2, 0)):
-            cost = []
-            for i in s1:
-                row = []
-                for j in s2:
-                    d = torch.squeeze(torch.norm((i - j), 2))
-                    row.append(d)
-                row = torch.squeeze(torch.stack(row))
-                cost.append(row)
-            cost = torch.stack(cost, 0).numpy()
-            row_ind, col_ind = linear_sum_assignment(cost)
-            row_remain = set(range(cost.shape[0])) - set(row_ind)
-            col_remain = set(range(cost.shape[1])) - set(col_ind)
+        zero, cost = torch.zeros_like(seq1[0, 0, :]), []
+        # Calculate similarity matrix
+        for i in range(seq1.shape[1]):
+            row = []
+            for j in range(seq2.shape[1]):
+                a, b = seq1[:, i, :], seq2[:, j, :]
+                d = self.cosine(a, b)
+                row.append(d)
+            row = torch.stack(row, 1)
+            cost.append(row)
+        cost = torch.stack(cost, 1)
+        # reorder batch items
+        reordered = []
+        for batchindex, matrix in enumerate(torch.unbind(cost, 0)):
+            row_ind, col_ind = linear_sum_assignment(matrix)
+            row_remain = set(range(matrix.shape[0])) - set(row_ind)
+            col_remain = set(range(matrix.shape[1])) - set(col_ind)
             row_ind = list(row_ind) + list(sorted(row_remain))
             col_ind = list(col_ind) + list(sorted(col_remain))
-            row = torch.stack([s1[i, :] for i in row_ind], 0)
-            R1.append(row)
+            maxl = max(len(row_ind), len(col_ind))
+            col_ind += [None] * (maxl - len(col_ind))
+            row_ind += [None] * (maxl - len(row_ind))
+            s1, s2 = seq1[batchindex, :, :], seq2[batchindex, :, :]
 
-        reorder = torch.stack(R1, 0)
-        return reorder
+            row = torch.stack([(s1[i, :] if i is not None else zero)
+                               for i in row_ind], 0)
+            col = torch.stack([(s2[i, :] if i is not None else zero)
+                               for i in col_ind], 0)
+            cat = torch.cat([row, col], 1)
+            reordered.append(cat)
+        reordered = torch.stack(reordered, 0)
+        return reordered
 
 
 if __name__ == '__main__':
